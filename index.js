f11a639be049c5f21425c48a492cfb2f82175c60
Luís Plinio <lpcampanholi@gmail.com>
2024-04-30 16:35:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+import puppeteer from 'puppeteer'
+
+async function robo() {
+
+  const browser = await puppeteer.connect({
+    browserURL: 'http://localhost:9222',
+  });
+
+  const page = await browser.newPage()
+
+  //const loginClikUp = 'luis.campanholi@viseueditores.com.br'
+  //const senhaClikUp = 'Zoh27752'
+
+  //const loginGoogle = 'luiscampanholiviseu@gmail.com'
+  //const senhaGoogle = ''
+  
+
+  // Set screen size
+  await page.setViewport({width: 1080, height: 1024});
+  
+  // Login no ClikUp
+  //await page.goto('https://app.clickup.com/login')
+  //await page.type('#login-email-input', loginClikUp)
+  //await page.type('#login-password-input', senhaClikUp)
+  //await page.click('[type="submit"]')
+  //await page.waitForNavigation();
+
+  // Task do Livro
+  await page.goto('https://app.clickup.com/t/befhea')
+
+  // Link do Drive
+  await page.waitForSelector('.cu-custom-fields__header-items-value__url', { visible: true })
+  const linkDrive = await page.evaluate(() => {
+      const element = document.querySelector(".cu-custom-fields__header-items-value__url")
+      return element ? element.href : null;  // Retorna o href se o elemento existir
+  });
+
+  //Acessa o Drive
+  //await page.goto(linkDrive)
+  //await page.type('#identifierId', loginGoogle)
+  //await page.click('.VfPpkd-RLmnJb')
+
+
+  // await browser.close();
+
+}
+
+robo();
+
+
